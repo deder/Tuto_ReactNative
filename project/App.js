d881218790ca4e0f4f860ba74062c76df35412ac
fcosta <fcosta@deder.fr>
2018-09-04 16:05:29 +0200

    création et Intégration du header

--- project/App.js
+++ project/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Header from './components/header/index';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test Fred</Text>
-      </View>
+
+        <Header content="Liste de taches" />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ project/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+import { style } from './style';
+const Header = ({content}) => ( 
+    <View>
+        <View />
+        <View >    
+            <Text >{content}</Text>
+        </View>
+    </View>
+)
+
+export default Header;
