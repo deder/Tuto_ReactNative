38122e5925847f347e391358cb27e1954bd6004d
fcosta <fcosta@deder.fr>
2018-09-10 15:31:19 +0200

    Rendu dynamique de la valeur du badge et rendu dynamique du style en lui passant les props

--- project/composants/task-list/list-item/index.js
+++ project/composants/task-list/list-item/index.js
@@ -3,30 +3,36 @@ import  { View } from 'react-native'
 import { ListItem as ListItemMaterial, Icon, Badge,Button, Avatar } from 'react-native-material-ui';   
 import { style } from './style';
 class ListItem extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            style : style(this.props)
+        }
+    }
     renderIcon = () => {
         return (<Icon name={this.props.iconName} style={{marginRight:10}} />)      
     }
     renderBadge = () =>(
-        <Button primary text="En cours" disabled style={style.badge}/>
+        <Button primary text={this.props.badgeName} disabled style={this.state.style.badge}/>
     )
     renderAvatar = () =>(
         <Avatar text="TE" size={40} />
     )
     renderRightElement = () => (
-        <View style={style.centerElement}>
+        <View style={this.state.style.centerElement}>
             {this.renderBadge()}
             {this.renderIcon()}   
         </View>
     )
     renderLeftElement = () => (
-        <View style={style.centerElement}>
+        <View style={this.state.style.centerElement}>
             {this.renderAvatar()}
         </View>
     )
     render() {
         return (
             <ListItemMaterial
-                style={style.listItemMaterial}    
+                style={this.state.style.listItemMaterial}    
                 divider
                 centerElement={this.props.children}
                 rightElement={this.renderRightElement()} 
