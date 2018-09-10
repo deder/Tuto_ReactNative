b69db428c01cfb44a388c4785f9ad1759b4eec9c
fcosta <fcosta@deder.fr>
2018-09-10 14:59:41 +0200

    Création du composant task list

--- project/composants/task-list/index.js
+++ project/composants/task-list/index.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { FlatList , StatusBar, View, Text} from 'react-native';
-import { ListItem, ThemeContext, getTheme, Toolbar,Icon  } from 'react-native-material-ui';  
+import { ThemeContext, getTheme} from 'react-native-material-ui';  
 import { APP_COLORS } from './../../styles/color';
+import ListItem from './list-item';
 import List from './list';
 
 const uiTheme = {
     palette: {
-      primaryColor: APP_COLORS.primary, 
+        primaryColor: APP_COLORS.primary, 
     }
-  };
-    
+};
 const TaskList = ({items}) => {
-    const renderItemList = ({item})=>{
-        console.log(item);
-        return (<List key={item.key} iconName={item.iconName}>{item.text}</List>)    
+    const pressHandler = (item)=>(event)=>{
+        console.log(item);      
     }
     return (
         <ThemeContext.Provider value={getTheme(uiTheme)}>  
-            <FlatList style={{
-                    padding:20,
-                    backgroundColor:"#ffffff" 
-                }}
-                data={items}    
-                renderItem={renderItemList}
-            />
-        </ThemeContext.Provider>  
+            <List>
+                {
+                    items.map((item)=>(
+                        <ListItem key={item.key} onClick iconName={item.iconName} onPress={pressHandler(item)}>{item.text}</ListItem>
+                    ))  
+                }  
+            </List>
+        </ThemeContext.Provider>
     )
-} 
-
+}
 export default TaskList
