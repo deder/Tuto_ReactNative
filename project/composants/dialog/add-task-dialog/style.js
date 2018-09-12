5f84bcfed1ba4aec5e4d0593702d5cfa5772bb49
fcosta <fcosta@deder.fr>
2018-09-12 17:02:30 +0200

    Création du composant AddTaskDialog

---
+++ project/composants/dialog/add-task-dialog/style.js
@@ -0,0 +1,15 @@
+
+import { APP_COLORS } from './../../../styles/color';
+
+export const styleLight = {
+    buttonChangeStatus: {
+        text:{
+            color: APP_COLORS.primaryAction,   
+        }
+    },
+    buttonDelete:{
+        text:{
+            color: 'red'
+        }
+    }
+}
