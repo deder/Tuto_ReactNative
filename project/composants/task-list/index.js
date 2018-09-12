be485def35a6e342ca67b8070a266a8111ae5440
fcosta <fcosta@deder.fr>
2018-09-12 10:31:57 +0200

    Fix Mise en page de task-list

--- project/composants/task-list/index.js
+++ project/composants/task-list/index.js
@@ -1,47 +1,47 @@
 import React from 'react';
-import { ThemeContext, getTheme, ActionButton} from 'react-native-material-ui';  
+import { ThemeContext, getTheme, ActionButton } from 'react-native-material-ui';
 import { APP_COLORS } from './../../styles/color';
 import ListItem from './list-item';
 import List from './list';
 
 const uiTheme = {
     palette: {
-        primaryColor: APP_COLORS.primary, 
+        primaryColor: APP_COLORS.primary,
     }
 };
-const TaskList = ({items, onPressListItem, onLongPressListItem, onPressBtnAction}) => {
+const TaskList = ({ items, onPressListItem, onLongPressListItem, onPressBtnAction }) => {
 
     return (
-        <ThemeContext.Provider value={getTheme(uiTheme)}>  
+        <ThemeContext.Provider value={getTheme(uiTheme)}>
             <List>
                 {
-                    items.map((item)=>(
-                        <ListItem 
-                            key={item.key} 
-                            onClick iconName={item.iconName} 
+                    items.map((item) => (
+                        <ListItem
+                            key={item.key}
+                            onClick iconName={item.iconName}
                             badgeName={item.badgeName}
                             onPress={onPressListItem(item)}
                             onLongPress={onLongPressListItem(item)}
                         >
                             {item.text}
                         </ListItem>
-                    ))      
-                }  
+                    ))
+                }
             </List>
             <ActionButton
                 actions={[{ icon: 'add', label: 'Créer une tache' }]}
-                icon="reorder"   
+                icon="reorder"
                 transition="speedDial"
                 onPress={onPressBtnAction}
                 style={
                     {
-                        container:{
-                            backgroundColor:APP_COLORS.primaryAction
+                        container: {
+                            backgroundColor: APP_COLORS.primaryAction
                         }
                     }
                 }
             />
-        </ThemeContext.Provider>     
+        </ThemeContext.Provider>
     )
 }
 export default TaskList
