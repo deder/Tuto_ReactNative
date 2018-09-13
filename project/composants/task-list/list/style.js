1f7507f60ccab6b0b889021b6ff699ee9ef75796
fcosta <fcosta@deder.fr>
2018-09-13 15:35:18 +0200

    Fix bug implémentation

--- project/composants/task-list/list/style.js
+++ project/composants/task-list/list/style.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
-export const style = StyleSheet.create({
+export const style = {
     flatList:{
         backgroundColor:"#ffffff"
     }
-});     
+};     
