3af36f8645a3af32b1ba0eb9afca6ea00d5c7b49
fcosta <fcosta@deder.fr>
2018-09-17 15:10:39 +0200

    publish sur expo

--- project/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ project/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -30,10 +30,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@fcosta/Project/index.exp", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40fcosta%2FProject%2F1.0.0%2F6db97aa6b744b4baacd5e11758dc7e0f-29.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40fcosta%2FProject%2F1.0.0%2Fdd2c5f7dc70fc98d2b87d882922f319f-29.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
